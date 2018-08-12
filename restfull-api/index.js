2947fa717eaecb8ed7566beb4e68677424b7c154
Arjen Smit <arjen@alnitak.nl>
2018-08-12 20:58:27 +0200

    stringDecoder

--- restfull-api/index.js
+++ restfull-api/index.js
@@ -6,8 +6,9 @@
 
 const http = require('http');
 const url = require('url');
+const stringDecoder = require('string_decoder').StringDecoder;
 
-const server = http.createServer(function(req, res){
+const server = http.createServer((req, res) => {
 
     // Get the URL and parse it
     const parsedUrl = url.parse(req.url, true);
@@ -18,11 +19,32 @@ const server = http.createServer(function(req, res){
     // const trimmePath = path.replace(/^\/+|\/+$/g, '');
     const trimmePath = path.replace(/\/+$/g, '');
 
-    // Send respons
-    res.end('Hello World!\n');
+    // Get the query string as an object.
+    const queryStringObject = parsedUrl.query;
 
-    // Log the request path
-    console.log('Request received on path: ' + trimmePath);
+    // Get the HTTP Method
+    const method = req.method.toUpperCase();
+
+    // Get the headers as an object
+    const headers = req.headers;
+
+    // Get the payload, if any
+    const decoder = new stringDecoder('utf-8');
+    let buffer = '';
+
+    req.on('data', (data) => {
+        buffer += decoder.write(data);
+    });
+
+    req.on('end', () => {
+        buffer += decoder.end();
+
+        // Send response
+        res.end('Hello World!\n');
+
+        // Log the request path
+        console.log(trimmePath, method, queryStringObject, headers, buffer);
+    });
 });
 
 server.listen(3000, function() {
